eac8bc48caf93968741aa5745bd5bbb36d171d21
xiao_Yang <643017440@qq.com>
2025-08-07 20:00:52 +0800

    feat(ref): implement toRef function and add ObjectRefImpl class for object property references

--- packages/reactivity/src/ref.ts
+++ packages/reactivity/src/ref.ts
@@ -1,5 +1,5 @@
 import { activeSub } from './effect'
-import { Link, link, propagate, Dependency } from './system'
+import { Dependency, link, Link, propagate } from './system'
 import { hasChanged, isObject } from '@vue/shared'
 import { reactive } from './reactive'
 
@@ -20,7 +20,7 @@ export interface Ref<T = any> extends Dependency {
  * Ref 的类
  */
 class RefImpl<T> implements Ref<T>, Dependency {
-  _value: T // 保存实际的值
+  _value: T; // 保存实际的值
   [ReactiveFlags.IS_REF] = true as const // ref 标记，是否是一个 ref
   subs: Link // 订阅者链表的头节点
   subsTail: Link // 订阅者链表的尾节点
@@ -39,8 +39,29 @@ class RefImpl<T> implements Ref<T>, Dependency {
   set value(newValue: T) {
     if (hasChanged(newValue, this._value)) {
       // 派发更新
-      this._value = (isObject(newValue) ? reactive(newValue as object) : newValue) as T
+      this._value = (
+        isObject(newValue) ? reactive(newValue as object) : newValue
+      ) as T
       triggerRef(this)
+    }
+  }
+}
+
+class ObjectRefImpl<T extends object, K extends keyof T> {
+  [ReactiveFlags.IS_REF] = true as const
+
+  constructor(
+    public _object: T,
+    public _key: K
+  ) {}
+
+  get value(): T[K] {
+    return this._object[this._key]
+  }
+
+  set value(newVal: T[K]) {
+    if (hasChanged(newVal, this._object[this._key])) {
+      this._object[this._key] = newVal
     }
   }
 }
@@ -55,6 +76,9 @@ export function isRef(value: any): value is Ref {
   return !!(value && value[ReactiveFlags.IS_REF])
 }
 
+export function toRef<T extends object, K extends keyof T>(target: T, key: K) {
+  return new ObjectRefImpl(target, key)
+}
 /**
  * 收集依赖，建立 ref 和 effect 之间的链表关联关系
  * @param dep 依赖项
