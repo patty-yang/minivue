09e29e0a8383b78f19497f477a39db3dc914ccb4
xiao_Yang <643017440@qq.com>
2025-08-19 18:08:27 +0800

    refactor(shared): rename

--- packages/shared/src/index.ts
+++ packages/shared/src/index.ts
@@ -1,17 +1,2 @@
-export function isObject(value) {
-  return typeof value === 'object' && value !== null
-}
-
-export function hasChanged(newValue, oldValue) {
-  return !Object.is(oldValue, newValue)
-}
-
-export function isFunction(value) {
-  return typeof value === 'function'
-}
-
-export function isOn(key) {
-  return /^on[A-Z]/.test(key)
-}
-
-export const isArray = Array.isArray
+export * from './utils'
+export * from './shapeFlags'
