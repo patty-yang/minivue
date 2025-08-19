13eab30a1d3fdea9bba6b623acde120bb00deb60
xiao_Yang <643017440@qq.com>
2025-08-19 18:08:34 +0800

    refactor(shared): rename

---
+++ packages/shared/src/utils.ts
@@ -0,0 +1,17 @@
+export function isObject(value) {
+  return typeof value === 'object' && value !== null
+}
+
+export function hasChanged(newValue, oldValue) {
+  return !Object.is(oldValue, newValue)
+}
+
+export function isFunction(value) {
+  return typeof value === 'function'
+}
+
+export function isOn(key) {
+  return /^on[A-Z]/.test(key)
+}
+
+export const isArray = Array.isArray
